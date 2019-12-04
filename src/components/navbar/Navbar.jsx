a2d2c94c7b72cef4f7e0694788b712f1a1ef8a03
MultipleIllusionsi <seregaserega06@gmail.com>
2019-12-04 17:51:02 +0500

    added hamburger navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink } from "react-router-dom";
 import { withRouter } from "react-router-dom";
@@ -6,6 +6,8 @@ import { withRouter } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = ({ location: { pathname } }) => {
+  const [isOpen, setOpen] = useState(false);
+
   let style =
     pathname.includes("album") ||
     pathname.includes("playlist") ||
@@ -16,10 +18,22 @@ const Navbar = ({ location: { pathname } }) => {
   return (
     <header className="header">
       <nav className="navbar">
-        <ul className="navbar__list" style={style}>
-          <li className="navbar__list-item logo">
-            <NavLink to="/">Galaxy</NavLink>
-          </li>
+        <div className="logo">
+          <NavLink to="/">Galaxy</NavLink>
+        </div>
+        <button
+          onClick={() => setOpen(!isOpen)}
+          className="hamburger"
+          aria-label="Open the menu"
+        >
+          <span></span>
+        </button>
+        <ul
+          className={`navbar__list ${
+            isOpen ? "navbar__list--opened" : ""
+          } `}
+          style={style}
+        >
           <li className="navbar__list-item">
             <NavLink to="/browse">Browse</NavLink>
           </li>
