6f0c2296a6935cf5cde021c21966c329aa89435b
MultipleIllusionsi <seregaserega06@gmail.com>
2019-12-06 00:39:05 +0500

    rewrite two class components w/ hooks

--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,61 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import "./CustomSelect.scss";
 
-class CustomSelect extends Component {
-  state = {
-    expanded: false,
-    currentSelectOption: "All",
+const CustomSelect = ({ option }) => {
+  const [open, setOpen] = useState(false);
+  const [currOption, setCurrOption] = useState("All");
+
+  const chooseSelectOption = e => {
+    setCurrOption(e.target.dataset.value);
+    setOpen(!open);
   };
 
-  toggleSelect = () => {
-    this.setState({ expanded: !this.state.expanded });
-  };
-
-  chooseSelectOption = e => {
-    this.setState({ currentSelectOption: e.target.dataset.value });
-    this.toggleSelect();
-  };
-
-  sendOption(e) {
+  const sendOption = e => {
     e.stopPropagation();
-    this.props.option(e.target.dataset.value);
-  }
+    option(e.target.dataset.value);
+  };
 
-  render() {
-    const { expanded, currentSelectOption } = this.state;
-
-    return (
-      <ul className="select">
-        <div onClick={this.toggleSelect} className="checked-option">
-          <p>{currentSelectOption}</p>
-          <p>&#9662;</p>
-        </div>
-        <ol
-          className={`option-list ${
-            expanded === true ? "open" : "close"
-          }`}
-          onClick={e => {
-            this.chooseSelectOption(e);
-            this.sendOption(e);
-          }}
-        >
-          <li data-value="Track" className="option-list__item">
-            Track
-          </li>
-          <li data-value="Artist" className="option-list__item">
-            Artist
-          </li>
-          <li data-value="Label" className="option-list__item">
-            Label
-          </li>
-          <li data-value="All" className="option-list__item">
-            All
-          </li>
-        </ol>
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="select">
+      <div onClick={() => setOpen(!open)} className="checked-option">
+        <p>{currOption}</p>
+        <p>&#9662;</p>
+      </div>
+      <ol
+        className={`option-list ${open === true ? "open" : "close"}`}
+        onClick={e => {
+          chooseSelectOption(e);
+          sendOption(e);
+        }}
+      >
+        <li data-value="Track" className="option-list__item">
+          Track
+        </li>
+        <li data-value="Artist" className="option-list__item">
+          Artist
+        </li>
+        <li data-value="Label" className="option-list__item">
+          Label
+        </li>
+        <li data-value="All" className="option-list__item">
+          All
+        </li>
+      </ol>
+    </ul>
+  );
+};
 
 export default CustomSelect;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,83 +1,67 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Spinner from "../../components/spinner/Spinner";
 import ArtistItem from "../../components/ArtistItem/ArtistItem";
 
 import "./Slider.scss";
 
-class Slider extends Component {
-  state = {
-    currentObj: this.props.initialObj,
-    total: this.props.total,
-  };
+const Slider = ({ data, total, initialObj }) => {
+  const [currObj, setCurrObj] = useState(initialObj);
 
-  btnClick = e => {
-    const { currentObj, total } = this.state;
+  const btnClick = e => {
     const { type } = e.target.dataset;
+
     if (type === "left") {
-      if (currentObj > 1) {
-        this.setState((prevState, _props) => ({
-          currentObj: prevState.currentObj - 1,
-        }));
-      } else if (currentObj === 1) {
-        this.setState(() => ({
-          currentObj: total,
-        }));
+      if (currObj > 1) {
+        setCurrObj(prevCount => prevCount - 1);
+      } else if (currObj === 1) {
+        setCurrObj(total);
       }
     }
 
     if (type === "right") {
-      if (currentObj < total) {
-        this.setState((prevState, _props) => ({
-          currentObj: prevState.currentObj + 1,
-        }));
-      } else if (currentObj === total) {
-        this.setState(() => ({
-          currentObj: 1,
-        }));
+      if (currObj < total) {
+        setCurrObj(prevCount => prevCount + 1);
+      } else if (currObj === total) {
+        setCurrObj(1);
       }
     }
   };
 
-  render() {
-    const { currentObj } = this.state;
-    const { data } = this.props;
-    console.log("---render from slider---");
-    return (
-      <div className="slider-wrapper">
-        <div className="slider-control">
-          <button
-            onClick={this.btnClick}
-            data-type="left"
-            className="slider-button slider-button__left"
-          ></button>
-          <button
-            onClick={this.btnClick}
-            data-type="right"
-            className="slider-button slider-button__right"
-          ></button>
-        </div>
-
-        {!data ? (
-          <Spinner />
-        ) : (
-          data.map(item => (
-            <ArtistItem
-              sliderClass={
-                currentObj === item.position
-                  ? "visible"
-                  : currentObj > item.position
-                  ? "invisible-left"
-                  : "invisible-right"
-              }
-              data={item}
-              key={item.id}
-            />
-          ))
-        )}
+  return (
+    <div className="slider-wrapper">
+      <div className="slider-control">
+        <button
+          onClick={btnClick}
+          data-type="left"
+          className="slider-button slider-button__left"
+        ></button>
+        <button
+          onClick={btnClick}
+          data-type="right"
+          className="slider-button slider-button__right"
+        ></button>
       </div>
-    );
-  }
-}
+
+      {!data ? (
+        <Spinner />
+      ) : (
+        data.map(item => (
+          <ArtistItem
+            sliderClass={
+              currObj === item.position
+                ? "visible"
+                : currObj > item.position
+                ? "invisible-left"
+                : "invisible-right"
+            }
+            data={item}
+            key={item.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Slider;
