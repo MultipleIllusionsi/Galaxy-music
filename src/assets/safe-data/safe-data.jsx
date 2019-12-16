9babf4a3a8ddfd47db76c1b22fbebe39809902c1
MultipleIllusionsi <seregaserega06@gmail.com>
2019-12-17 01:09:23 +0500

    deleted unnecessary file

--- src/assets/safe-data/safe-data.jsx
+++
@@ -1,9 +0,0 @@
-export default {
-  topArtists: {
-    picture_xl:
-      "http://e-cdn-images.deezer.com/images/artist/718942059d7b35b22a40da0d23554e8d/1000x1000-000000-80-0-0.jpg",
-    name: "Post Malone",
-  },
-  topPlaylists: null,
-  latestTrack: null,
-};
